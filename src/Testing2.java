4e91bc1f140fea2eb6a871700228900188f9145e
SailpointRakesh <rakeshsailpoint123@gmail.com>
2023-08-05 02:11:26 +0530

    committing test2 to branch x

---
+++ src/Testing2.java
@@ -0,0 +1,12 @@
+
+public class Testing2 {
+
+	public Testing2() {
+		// TODO Auto-generated constructor stub
+	}
+public static void main(String[] args) {
+	int a=10;
+	if(a>=10)
+		System.out.println("a is greater than a");
+}
+}

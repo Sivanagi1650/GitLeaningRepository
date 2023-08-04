421959ec8f2e7e7ccdf69ba3eb03acc519f3bbea
SailpointRakesh <rakeshsailpoint123@gmail.com>
2023-08-05 01:31:00 +0530

    First commit................

---
+++ src/Testing.java
@@ -0,0 +1,12 @@
+
+public class Testing {
+
+	public Testing() {
+		// TODO Auto-generated constructor stub
+	}
+public static void main(String[] args) {
+	int a=10;
+	if(a>=10)
+		System.out.println("a is greater than a");
+}
+}
